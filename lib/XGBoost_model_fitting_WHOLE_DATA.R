845308784d615990d2384927886148a606a7be8d
William Raikes <wraikes@hotmail.com>
2016-12-07 14:01:01 -0500

    Add files via upload

---
+++ lib/XGBoost_model_fitting_WHOLE_DATA.R
@@ -0,0 +1,51 @@
+library(dplyr)
+library(xgboost)
+
+setwd('C:\\Users\\LENOVO\\Desktop\\Academic\\ADS\\project_5')
+load('df(final).RData')
+
+train_df <- df
+
+
+for (i in c(3, 4, 5, 12, 13, 14, 15, 17, 18, 21, 24)){
+  train_df[, i] <- as.factor(train_df[ , i])
+}
+
+###Train the model
+train_df_X <- data.matrix(train_df[ , c(3:6, 8:15, 17:24)])
+
+cv_number <- nrow(train_df_X)
+k_folds <- cut(1:cv_number, breaks = 5, labels = FALSE)
+cv_errors <- matrix(0, nrow = 24, ncol = 5)
+
+
+for (k in 1:5){
+  models <- list()
+  cv_indices <- which(k_folds == k, arr.ind = TRUE)
+  cv_train <- train_df_X[cv_indices, ]
+  cv_test <- train_df_X[-cv_indices, ]
+  
+  for (i in 25:48){
+    cv_y <- train_df[cv_indices , i]
+    temp_model <- xgboost(data = cv_train, 
+                          label = cv_y, 
+                          nrounds = 50,
+                          missing = NaN
+    )
+    models[[i - 24]] <- temp_model
+    print(i)
+  }
+  
+  pred_matrix <- matrix(0, ncol = 24, nrow = nrow(cv_test))
+  error_rate <- rep(0, 24)
+  
+  for (j in 1:24){
+    
+    pred <- round(predict(models[[j]], cv_test, missing = NaN))
+    pred_matrix[ , j] <- pred
+    error_rate[j] <- sum(pred != train_df[-cv_indices , j + 24]) / length(pred)
+    
+    print(j)
+  }
+  cv_errors[, k] <- error_rate
+}
